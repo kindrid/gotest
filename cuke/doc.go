cbeaae9ff51b4f8e7dd4d0ce6e8e0c09e20a2380
bradobro <brad.olson@movedbylight.com>
2017-03-30 17:39:28 -0400

    Completely removed cucumber directory now that GoDog is more capable

--- cuke/doc.go
+++
@@ -1,7 +0,0 @@
-/*Package cuke provides Cucumber (Gherkin) testing helpers.
-
-Gotest/cuke is still experimental and the API may change drastically.
-
-Current Cucumber Implementation: github.com/DATA-DOG/godog
-*/
-package cuke
